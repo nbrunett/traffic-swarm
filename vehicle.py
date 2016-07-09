e6ddcfd6e8b4faad4bdaab43b212286b3617a374
nbrunett <nate.brunetti@gmail.com>
2016-07-09 13:08:54 -0400

    add parallel drag (e.g. "air drag")

--- vehicle.py
+++ vehicle.py
@@ -4,7 +4,7 @@ transFric = 0.1
 
 class vehicle:
     def __init__(self, width, length, height, weight, nWheels, maxSpeed, \
-                 maxAccel, maxFuel, fuel, direction, pos, vel):
+                 maxAccel, maxFuel, fuel, fric, direction, pos, vel):
         self.width = width
         self.length = length
         self.height = height
@@ -14,39 +14,48 @@ class vehicle:
         self.maxAccel = maxAccel
         self.maxFuel = maxFuel
         self.fuel = fuel
+        self.fric = fric
         self.direction = direction #north of east; radians
         self.pos = pos
         self.vel = vel
         self.speed = np.linalg.norm(self.vel)
 
+
     def changePosition(self, accel, dt):
-        #find unit vector perpendicular to direction
+        #calculate parallel drag (e.g. "air drag")
         dirVec = np.array([np.cos(self.direction), np.sin(self.direction)])
+        parVel = np.dot(self.vel, dirVec)
+        parDrag = -parVel*dirVec*self.fric
+
+        #calculate transverse drag
         if dirVec[0] == 0.0:
             perp = np.array([1.0, -dirVec[0]/dirVec[1]])
         else:
             perp = np.array([-dirVec[1]/dirVec[0], 1.0])
         perp /= np.linalg.norm(perp)
         transVel = np.dot(self.vel, perp)
-        transCorr = -transVel*perp*transFric
-        
+        transDrag = -transVel*perp*transFric
+
         #kick velocity
         accel = np.array(accel)
-        self.vel += (dt/2.0)*accel + transCorr
+        self.vel += (dt/2.0)*accel + parDrag + transDrag
         #drift position
         self.pos += dt*self.vel
         #kick velocity
-        self.vel += (dt/2.0)*accel + transCorr
+        self.vel += (dt/2.0)*accel + parDrag + transDrag
 
         self.speed = np.linalg.norm(self.vel)
 
+
     def changeDirection(self, delDirection):
         self.direction += delDirection
+
 
     def burnFuel(self, fuelBurnt):
         #could include decreasing weight by fuel amount
         #this all could be quite complicated if it wasn't me programming it
         self.fuel -= fuelBurnt
+
 
     def getOnHighway(self, start, end, dt):
         finalSpeed = 29.06

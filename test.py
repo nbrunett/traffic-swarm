3287e9df131790a341e5e3323dab26d2b821af1f
nbrunett <nate.brunetti@gmail.com>
2016-07-09 21:28:35 -0400

    update getOnHighway test and tweak route endpoints

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 import route
 import vehicle
 
-'''
-car = vehicle.vehicle(7, 10, 5, 3200, 4, 30, 4, 12, 30, 0, 0, 0, 0, 0)
+car = vehicle.vehicle(7, 10, 5, 3200, 4, 31.29, 9.8, 12, 12, 0.0, np.pi/4.0, \
+                      [0, 0], [10, 20])
 
-print car.x
-print car.y
-car.getOnHighway([car.x, car.y], [50, 50], 0.01)
-print car.vx
-print car.vy
+print car.pos
+car.getOnHighway(car.pos, np.array([50, 50]), 0.01)
+print car.vel
 print car.speed
-'''
 
-x = route.route(42.169002, -72.326345, 42.210410, -71.784496)
-print x.coords.shape
-print x.coords[0]
+
+x = route.route(42.170350, -72.479596, 42.210243, -71.784453)
+plt.plot(x.coords[:, 0], x.coords[:, 1])
+plt.scatter(x.coords[:, 0], x.coords[:, 1])
+plt.show()
